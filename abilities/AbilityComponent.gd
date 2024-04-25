7585dc7103e20779099462bdcc221dbea1824339
Antoine PARIZE <45354189+lejugeti@users.noreply.github.com>
2024-04-25 21:47:38 +0200

    Prevent ability execution if required tags aren't present on other activated abilities

--- abilities/AbilityComponent.gd
+++ abilities/AbilityComponent.gd
@@ -5,24 +5,57 @@ extends Node
 var _abilities: Array[Ability] = []
 
 
-func add_ability(ability: Ability):
+func add_ability(ability: Ability) -> void:
     var already_present: bool = _abilities.any(ability.equals)
     if(!already_present):
         _abilities.append(ability)
 
 
-func remove_ability(ability: Ability):
+func remove_ability(ability: Ability) -> void:
     _abilities.erase(ability)
 
 
-func activate_by_name(ability_name: String):
+func activate_by_name(ability_name: String) -> void:
     for ability in self._abilities:
         if(ability.get_ability_name() == ability_name):
-            ability.execute()
+            try_activate_ability(ability)
 
 
 ## Activates all abilities that share the same tag passed as argument
-func activate_by_tag(tag: Tag):
+func activate_by_tag(tag: Tag) -> void:
     for ability in self._abilities:
         if(ability.tag.equals(tag)):
-            ability.execute()
+            try_activate_ability(ability)
+
+
+func try_activate_ability(ability: Ability) -> void:
+    if(can_be_activated(ability)):
+        ability.is_active = true
+        ability.execute()
+
+
+func can_be_activated(ability: Ability) -> bool:
+    return all_required_tags_are_present(ability)
+
+
+## Determines if all required tags for activation are present in active abilities
+func all_required_tags_are_present(ability: Ability) -> bool:
+    return ability.activation_required_tags.all(is_part_of_active_tags)
+
+
+func get_active_abilities() -> Array[Ability]:
+    return _abilities.filter(func(ability: Ability): return ability.is_active)
+
+
+func get_active_tags() -> Array[Tag]:
+    var activeTags: Array[Tag] = []
+    for activeAbility in get_active_abilities():
+        activeTags.append(activeAbility.tag)
+
+    return activeTags
+
+
+## True if the tag passed as argument matches an already active tag
+func is_part_of_active_tags(tag: Tag) -> bool:
+    assert(tag != null)
+    return get_active_tags().any(func(active: Tag): return active.equals(tag))
--- abilities/ability.gd
+++ abilities/ability.gd
@@ -5,6 +5,13 @@ extends Node
 var ability_name: String
 var tag: Tag
 
+## Tags required in before activating this ability
+var activation_required_tags: Array[Tag] = []
+
+## If the ability is currently active or not.
+## One should plan to disable an ability after its job is done.
+var is_active = false
+
 
 func assert_valid_ability() -> void:
     assert(!ability_name.is_empty())
